9a9ee6135748188daf3629f85a9f43d1ffc58ba1
Steinway <adrian.montas232@gmail.com>
2023-07-31 16:06:57 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -23,3 +23,40 @@ def get_math_operation():
         if choice in ["1", "2", "3", "4"]:
             return int(choice)
         print("Invalid choice. Please try again.")
+
+def get_numbers():
+    while True:
+        try:
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input("Enter the second number: "))
+            return num1, num2
+        except ValueError:
+            print("Invalid input. Please enter valid numbers.")
+
+def calculator_app():
+    while True:
+        operation = get_math_operation()
+        num1, num2 = get_numbers()
+
+        if operation == 1:
+            result = add(num1, num2)
+        elif operation == 2:
+            result = subtract(num1, num2)
+        elif operation == 3:
+            result = multiply(num1, num2)
+        elif operation == 4:
+            try:
+                result = divide(num1, num2)
+            except ValueError as e:
+                print(f"Error: {e}")
+                continue
+
+        print(f"Result: {result}")
+
+        choice = input("Do you want to try again? (yes/no): ").lower()
+        if choice != "yes":
+            print("Thank you!")
+            break
+
+if __name__ == "__main__":
+    calculator_app()
