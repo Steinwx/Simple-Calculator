57e0da333f8b147da357d49b4f20be286e8e7f60
Steinway <adrian.montas232@gmail.com>
2023-07-31 16:01:03 +0800

    Adding basic calculator operations

--- calculator.py
+++ calculator.py
@@ -1,10 +1,13 @@
-Create a Simple App Calculator
-1.  The application will ask the user to choose one of the 
-      four math operations (Addition, Subtraction, Multiplication and Division)
-2.  The application will ask the user for two numbers
-3.  Display the result
-4. The application will ask if the user wants to try again or not.
-5. If yes, repeat Step 1.
-6. If no, Display “Thank you!” and the program will exit 
-7. Use Python Function and appropriate Exceptions to capture errors during   
-    runtime.
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
