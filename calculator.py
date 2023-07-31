1ab42648a09b62f311872bdc24b94f0d2bf01d10
Steinway <adrian.montas232@gmail.com>
2023-07-31 16:02:47 +0800

    Add printables

--- calculator.py
+++ calculator.py
@@ -11,3 +11,15 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
+def get_math_operation():
+    while True:
+        print("Choose a math operation:")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        choice = input("Enter the corresponding number (1/2/3/4): ")
+        if choice in ["1", "2", "3", "4"]:
+            return int(choice)
+        print("Invalid choice. Please try again.")
